5447ea3c4ce81b5cae138734cd547d3ba2843595
GamingCuber <thegamingcuber19@gmail.com>
2023-01-13 18:15:35 -0800

    Edited plugin.yml and Main.java for v1.0

--- src/main/java/me/gamingcuber/minecraftmanhunt/Main.java
+++ src/main/java/me/gamingcuber/minecraftmanhunt/Main.java
@@ -1,5 +1,11 @@
 package me.gamingcuber.minecraftmanhunt;
 
+import me.gamingcuber.minecraftmanhunt.commands.CompassCommand;
+import me.gamingcuber.minecraftmanhunt.commands.RunnerCommand;
+import me.gamingcuber.minecraftmanhunt.listeners.CompassClickListener;
+import me.gamingcuber.minecraftmanhunt.listeners.DeathListener;
+import me.gamingcuber.minecraftmanhunt.listeners.RespawnListener;
+import me.gamingcuber.minecraftmanhunt.tabcompleter.RunnerTabCompleter;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -14,6 +20,13 @@ public final class Main extends JavaPlugin {
     public Player Runner;
     @Override
     public void onEnable() {
+
+        new CompassCommand(this);
+        new RunnerCommand(this);
+        new CompassClickListener(this);
+        new DeathListener(this);
+        new RespawnListener(this);
+        new RunnerTabCompleter(this);
 
     }
 
